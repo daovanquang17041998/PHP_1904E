4bb105de7357919809497cbaa567310fb9a8c751
QUANG <aovanquang@gmail.com>
2019-08-10 09:44:45 +0700

    modau:

---
+++ PHP/001_vidu/index2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Tile</title>
+</head>
+<body>
+<pre>
+
+</pre>
+<?php
+echo  " <p style='color: #fd2a28'>Chào pho</p>";
+?>
+?>
+</body>
+
---
+++ PHP/001_vidu/index3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Tile</title>
+</head>
+<body>
+<pre>
+ Nhúng cú pháp php vào trong trong code html
+    Nhưng phải nhớ rằng đuôi file luôn luôn là .php
+    Cú pháp của php : ?php ?
+    - Lệnh echo trong php là lệnh xuất ra màn hình
+    - Các câu lệnh php phải luôn kết thúc bằng dấu ;
+    - echo thẻ html
+</pre>
+" <p style='color: #fd2a28'> echo "Chào php" </p>;
+?>
+</body>
+
+
